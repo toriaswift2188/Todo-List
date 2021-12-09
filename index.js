7ea0a047b08f44e3cc6573fc1de0cf22fc463c82
Victoria Swift <Victoriaswift2188@gmail.com>
2021-12-09 10:38:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-function App(){
-  const [todos, setTodos] = React.useState([
-    {
-      text: 'learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'meet friend for lunch',
-      isCompleted: false,
-    },
-    {
-      text: 'build todo app',
-      isCompleted: false,
-    }        
-  ]);
-
-  const addTodo = text => {
-    const newTodos = [...todos, {text, isCompleted:false}];
-    setTodos(newTodos);
-  }
-  const removeTodo = index => {
-    let temp = [...todos];    
-    temp.splice(index, 1);
-    setTodos(temp);
-  }
-
-  return(
-    <div className="app">
-      <div className="todo-list" >
-        {todos.map((todo, i) => (
-          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
-    </div>
-  );
-}
+import React from "react";
+import ReactDom from "react-dom";
+import "./styles.css";
+import App from './App'
 
 ReactDOM.render(
-  <App/>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
