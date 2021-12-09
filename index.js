a014408f1d68acc8fe8e9c3e90d79ba018ebc0db
Victoria Swift <Victoriaswift2188@gmail.com>
2021-12-09 10:17:00 -0500

    Update index.js
    Compiled .js files

--- index.js
+++ index.js
@@ -23,6 +23,36 @@ function App(){
   );
 }
 
+function TodoForm({addTodo}){
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="New To-Do..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+function Todo({todo,index,remove}){
+  function handle(){
+    console.log('Ping:',index);
+    remove(index);
+  }
+  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+}
+
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
