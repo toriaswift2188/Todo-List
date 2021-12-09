b85a286a8f3a9e8905c45cd42a6cc30b8905416d
Victoria Swift <Victoriaswift2188@gmail.com>
2021-12-09 10:18:10 -0500

    Delete Todo.js

--- Todo.js
+++
@@ -1,7 +0,0 @@
-function Todo({todo,index,remove}){
-  function handle(){
-    console.log('Ping:',index);
-    remove(index);
-  }
-  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
-}
